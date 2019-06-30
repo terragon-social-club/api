97fe327dccf51e207cca14239651846d8a4594b0
Mike Keen <mkeen.atl@gmail.com>
2019-06-30 10:29:05 -0400

    basic hooks for stripe + couch integration

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import Stripe from "stripe";
+import { of, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { Member } from './types';
 
@@ -6,37 +9,63 @@ import typesTI from "./types-ti";
 import { createCheckers } from "ts-interface-checker";
 const { Member } = createCheckers(typesTI);
 
-import { CouchDB, AuthorizationBehavior } from '@mkeen/rxcouch';
+import { CouchDB, AuthorizationBehavior, CouchDBDocument } from '@mkeen/rxcouch';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
 const couch = new CouchDB({
-  dbName: 'terragon',
-  host: 'couchdb-a.terragon.us',
-  port: 443,
-  ssl: true
+  dbName: 'users',
+  host: 'localhost',
+  port: 5984,
+  ssl: false,
+  trackChanges: false
 }, AuthorizationBehavior.cookie,
-  process.env.COUCH_USER,
-  process.env.COUCH_PASS,
+  of({
+    username: process.env.COUCH_USER,
+    password: process.env.COUCH_PASS
+  })
 );
 
 app.post('/user', (req: any, res: any) => {
   res.setHeader('Content-Type', 'application/json');
-  try {
-    Member.check(req.body);
-    const incoming: Member = req.body;
-    const doc = couch.doc(incoming)
-      .subscribe((_a: any) => {
-        res.end(JSON.stringify(_a));
-      });
+  Member.strictCheck(req.body);
+  const incoming: Member = req.body;
 
-  } catch (e) {
-    res.status(400);
-    res.end(JSON.stringify({}));
-  }
+  couch.find({
+    selector: {
+      phone: incoming.phone
+    }
+  }).subscribe((documents: CouchDBDocument[]) => {
+    if (documents.length === 0) {
+      couch.doc(incoming)
+        .pipe(take(1))
+        .subscribe((newDoc: CouchDBDocument) => {
+          stripe.customers.create({
+            email: newDoc.email,
+            metadata: {
+              userId: newDoc._id
+            }
+          }, function(err: any, customer: any) {
+            let doc = newDoc;
+            doc.stripe_id = customer.id;
+            couch.doc(doc).subscribe((savedDoc: CouchDBDocument) => {
+              res.end(JSON.stringify(['savedCustomer']))
+            }).unsubscribe()
+          });
+
+          //res.end(JSON.stringify(newDoc));
+        })
+        ;
+    } else {
+      res.end(JSON.stringify({}));
+    }
+
+  })
 
 });
 
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
 export interface Member {
+  _id?: string;
+  _rev?: string;
   first_name: string;
   last_name: string;
   handle: string;
   phone: string;
+  email: string;
 }
